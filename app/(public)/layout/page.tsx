d42858b87ac8c2e4e4c9506e31fe9cfd9d297124
Prathamesh Pawar <prathmeshpawar4169@gmail.com>
2026-01-23 09:31:51 +0530

    Removed empty ui page from public/ and moved to public/layout/

---
+++ app/(public)/layout/page.tsx
@@ -0,0 +1,13 @@
+import { Container, Box } from "@mui/material";
+import PublicTopbar from "../../components/blog/PublicNavbar";
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Box sx={{ minHeight: "100vh", bgcolor: "#fff" }}>
+      <PublicTopbar />
+      <Container maxWidth="lg" sx={{ py: 4 }}>
+        {children}
+      </Container>
+    </Box>
+  );
+}
